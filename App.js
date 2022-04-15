1b19bf123ae79ff2a7b8bd042b2669258eea838a
Neha Joshi <nehajoshigkp@gmail.com>
2022-04-15 22:40:29 +0530

    css added

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'https://api.coingecko.com/api/v3/companies/public_treasury/bitcoin',
+    }).then((response) => {
+      // console.log(response.data.companies);
+      setData(response.data.companies);
+      console.log(data)
+      // console.log(response.data.companies.map((a) => a.name))
+    });
+  },[])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Crypto Price Tracking</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +29,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header : {
+    position: 'relative',
+    top: 50,
+    fontSize: 20,
+    fontWeight: "bold"
+  }
 });
